699a5a60d93370470d96cc77937111511457be27
Abdenour KEDDAR <oussama.keddar@epitech.eu>
2023-07-03 13:00:54 +0200

    rename the folder to groups

--- group_dag.py
+++ group_dag.py
@@ -3,7 +3,7 @@ from airflow.operators.bash import BashOperator
 
 from datetime import datetime
 
-from subdags.group_downloads import download_tasks
+from groups.group_downloads import download_tasks
 
 with DAG('group_dag', start_date=datetime(2023, 7, 1),
          schedule_interval='@daily', catchup=False) as dag:
