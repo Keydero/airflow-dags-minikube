3880550c7e179bf3f9198096b6721fcd5006470a
Abdenour KEDDAR <oussama.keddar@epitech.eu>
2023-07-03 12:59:52 +0200

    Use TaskGroup instead of subdag

--- group_dag.py
+++ group_dag.py
@@ -1,18 +1,15 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.subdag import SubDagOperator
+
 from datetime import datetime
 
-from subdags.subdag_downloads import subdag_downloads
+from subdags.group_downloads import download_tasks
 
 with DAG('group_dag', start_date=datetime(2023, 7, 1),
          schedule_interval='@daily', catchup=False) as dag:
     args = {'start_date': dag.start_date, 'schedule_interval': dag.schedule_interval, 'catchup': dag.catchup}
 
-    downloads = SubDagOperator(
-        task_id='downloads',
-        subdag=subdag_downloads(dag.dag_id,'downloads', args)
-    )
+    downloads = download_tasks()
 
     check_files = BashOperator(
         task_id='check_files',
